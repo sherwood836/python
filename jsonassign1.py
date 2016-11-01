90e85ad35b62acfdc7d7cf93ddb7b3ddabc904be
Tommy Di <lgp.tommyd.1026@gmail.com>
2016-11-01 10:28:10 -0500

    Finished JSON assignment 1

--- jsonassign1.py
+++ jsonassign1.py
@@ -1,22 +1,15 @@
 import json
+import urllib
 
-input = '''
-[
-  { "id" : "001",
-    "x" : "2",
-    "name" : "Chuck"
-  } ,
-  { "id" : "009",
-    "x" : "7",
-    "name" : "Chuck"
-  } 
-]'''
+address = raw_input('Enter location: ')
 
-info = json.loads(input)
-print 'User count:', len(info)
+print 'Retrieving', address
+uh = urllib.urlopen(address)
+data = uh.read()
+print 'Retrieved',len(data),'characters'
 
-for item in info:
-    print 'Name', item['name']
-    print 'Id', item['id']
-    print 'Attribute', item['x']
+info = json.loads(data)
+
+print 'Count:', len(info["comments"])
+print 'Sum:', sum ([int(item["count"]) for item in info["comments"]])
 
